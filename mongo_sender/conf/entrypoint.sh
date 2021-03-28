88e08be019f6b7913c49c3276a1d383d1993bde1
Javier S.R <javier.sanchez.ruiz.jsr@gmail.com>
2021-03-28 22:57:50 +0200

    New: Mongo sender. Restore and backup

--- mongo_sender/conf/entrypoint.sh
+++ mongo_sender/conf/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# move to backups folder
-cd /backups
+# sleep some seconds
+sleep 5
 
 # restore backup
 if [ -d "./restore" ]; then
-    echo "Restoring backup in database ${ENVIRONMENT}"
-    mongorestore --host mongo -d ${ENVIRONMENT} ./restore
+    echo "Restoring backup in database ${DATABASE_NAME}"
+    mongorestore --host ${DATABASE_HOST} --port ${DATABASE_PORT} --db ${DATABASE_NAME} --username ${DATABASE_USER} --password ${DATABASE_PASSWORD} --authenticationDatabase admin ./restore
     rm -rf ./restore
 fi
+
+# move to backups folder
+cd /backups
 
 while [ true ]; do
 
@@ -18,33 +21,26 @@ while [ true ]; do
     echo "Starting backup: ${now}"
 
     echo "Executing database backup"
-    /usr/bin/mongodump --host mongo -d ${ENVIRONMENT} --port 27017 --out .
-    zip -r aluzina_${ENVIRONMENT}_${now}.dump.zip ${ENVIRONMENT}
-    rm -rf ${ENVIRONMENT}
+    /usr/bin/mongodump --host ${DATABASE_HOST} --port ${DATABASE_PORT} --db ${DATABASE_NAME} --username ${DATABASE_USER} --password ${DATABASE_PASSWORD} --authenticationDatabase admin --out .
+    zip -r ${ENVIRONMENT}_${DATABASE_NAME}_${now}.dump.zip ${DATABASE_NAME}
+    rm -rf ${DATABASE_NAME}
 
-    if [ "$hour" = "$BACKUP_FILES_HOUR" ]; then
-        echo "Executing admin backup"
-        zip -r aluzina_${ENVIRONMENT}_${now}.admin.zip /webservices
-
-        echo "Executing landing backup"
-        zip -r aluzina_${ENVIRONMENT}_${now}.landing.zip /landing
+    if [ "$BACKUP_SECRET" != "" ]; then
+        echo "Sending files to silicornio backup"
+        curl --location --request PUT "${BACKUP_RECEIVER}/data" --header "Authorization: ${BACKUP_SECRET}" --form "${ENVIRONMENT}_${DATABASE_NAME}_${now}.dump.zip=@\"./${ENVIRONMENT}_${DATABASE_NAME}_${now}.dump.zip\""
     fi
-
-    echo "Removing old files"
-    find /backups/ -mtime +$BACKUP_DAYS -exec rm -rf {} \;
-    echo "" > .gitkeep
 
     if [ "$S3_ACCESS_KEY" != "" ]; then
         echo "Syncing files to S3"
         aws configure set aws_access_key_id $S3_ACCESS_KEY
         aws configure set aws_secret_access_key $S3_SECRET_KEY
         aws configure set AWS_DEFAULT_REGION $S3_REGION
-        if [ "${ENVIRONMENT}" = "local" ]; then
-            aws s3 sync /backups s3://aluzina-test --delete
-        else
-            aws s3 sync /backups s3://aluzina-${ENVIRONMENT} --delete
-        fi
+        aws s3 sync /backups s3://${S3_BUCKET} --delete
     fi
+
+    echo "Removing old files"
+    find /backups/ -mtime +$BACKUP_DAYS -exec rm -rf {} \;
+    echo "" > .gitkeep
 
     echo "Sleeping"
     sleep $BACKUP_SLEEP
